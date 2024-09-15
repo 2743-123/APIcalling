1d279f3eb060a811e7a0151b924a369f7943518c
2743-123 <2743-123>
2024-09-15 09:25:05 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+.then(response=>response.json())
+.then(data=>{
+    console.log(data)
+})
+.catch(error=>{
+    console.log('Error:',error
+    )
+});
